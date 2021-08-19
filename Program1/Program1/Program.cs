b260e719418f229b6103ce00c860ba17bd5589b0
MD.SHAHZAD HUSSAIN RAYIED <76652122+Rayied991@users.noreply.github.com>
2021-08-19 20:35:32 +0600

    Add files via upload

---
+++ Program1/Program1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C-Sharp!");//Writing the output but not showing
+            Console.ReadLine();//Keep the console window up until press another button
+            
+            //Drawing a shape
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+            Console.ReadLine();
+
+        }
+    }
+}
