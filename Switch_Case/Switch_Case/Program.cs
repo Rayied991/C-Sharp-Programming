99667b66bc2f159cd7905b3efacf1e5ea3579b34
MD.SHAHZAD HUSSAIN RAYIED <76652122+Rayied991@users.noreply.github.com>
2021-08-25 16:12:18 +0600

    Add files via upload

---
+++ Switch_Case/Switch_Case/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Case
+{
+
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a day number:");
+            int day = Convert.ToInt32(Console.ReadLine());
+            switch (day)
+            {
+                /*When C# reaches a break keyword, it breaks out of the switch block
+                 This will stop the execution of more code and case testing inside the block.
+                 */
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                //The default keyword is optional and specifies some code to run if there is no case match
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+
+            Console.ReadLine();
+
+        }
+       
+    }
+}
