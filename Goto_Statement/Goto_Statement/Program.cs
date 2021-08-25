d3aa767f21a7334029e07408f54b84674f17bbd3
MD.SHAHZAD HUSSAIN RAYIED <76652122+Rayied991@users.noreply.github.com>
2021-08-25 17:47:31 +0600

    Add files via upload

---
+++ Goto_Statement/Goto_Statement/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goto_Statement
+{
+    class Program
+    {
+        /*
+         The goto is C#  unconditional jump statement. When encountered, program flow jumps to the location specified by the goto.
+
+                The goto requires a label of operation. A label is a valid C# identifier followed by colon.
+          */
+        static void Main(string[] args)
+        {
+            for(int i = 1; i < 5; i++)
+            {
+                switch (i)
+                {
+                    case 1:
+                        Console.WriteLine("In case 1");
+                        goto case 3;
+                    case 2:
+                        Console.WriteLine("In case 2");
+                        goto case 1;
+                    case 3:
+                        Console.WriteLine("In case 3");
+                        goto default;
+                    default:
+                        Console.WriteLine("In default");
+                        break;
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
