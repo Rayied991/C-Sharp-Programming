cd5f033fb970a18f7689e2e6831c2e6e0717d762
MD.SHAHZAD HUSSAIN RAYIED <76652122+Rayied991@users.noreply.github.com>
2021-11-13 20:12:28 +0600

    Add files via upload

---
+++ Collections/Stack_Queue/Stack_Queue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+namespace Stack_Queue
+{
+    class Program
+    {
+        //Colections
+        //3 category
+        //1.Generic Colllection
+        //2.Non-Generic Colllection
+        //3.Concurrent Colllection
+
+
+        //non-generic collections->Arraylist,Queue,Stack,Hashtable
+        //Generic Collections->List,Queue,Stack,SortedList,Dictionary
+        //These are already implemented inside the System.collections namespace
+
+        
+        //Queue
+        static void Main(string[] args)
+        {
+            //Stack(Push,Pop)
+            Stack s1 = new Stack();
+
+            //Push
+
+            s1.Push(5);
+            Console.ReadKey();
+        }
+    }
+}
