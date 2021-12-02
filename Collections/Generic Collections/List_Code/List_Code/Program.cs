37515e0b8207e344c0759c0c4e2e227a20620b95
MD.SHAHZAD HUSSAIN RAYIED <76652122+Rayied991@users.noreply.github.com>
2021-12-02 17:11:49 +0600

    Add files via upload

---
+++ Collections/Generic Collections/List_Code/List_Code/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_Code
+{
+    class students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double cgpa { get; set; }
+    }
+    class Program
+    {
+        //Generic Collections-> type safe
+        //list
+        //queue
+        //stack
+        //sortedlist<key,value>
+        //dictionary<key,value>
+        //hashset-> non-duplicate value
+
+        //User defined type list
+        
+        static void Main(string[] args)
+        {
+            //   Pre defined datatype of List
+            //List
+            Console.WriteLine("*************Pre-Defined type List*************");
+            List<int> List1 = new List<int>();
+
+            List1.Add(111);
+            //List1.Add(34.5);
+            List1.Add(1);
+            Console.WriteLine("Count of List1= " + List1.Count);
+            Console.WriteLine("Capacity of List1= " + List1.Capacity);
+            Console.WriteLine("List1 values:");
+            foreach (int i in List1)
+            {
+                Console.WriteLine(i);
+            }
+            List<string> list2 = new List<string>();
+            list2.Add("Hello");
+            Console.WriteLine("Count of List2= " + list2.Count);
+            Console.WriteLine("Capacity of List2= " + list2.Capacity);
+            Console.WriteLine("List2 values:");
+            foreach (string i in list2)
+            {
+                Console.WriteLine(i);
+            }
+
+            //User defined list
+            students s = new students();
+            s.Id = 1;
+            s.Name = "Rasel";
+            s.cgpa = 3.76;
+            Console.WriteLine("*************User-Defined type List*************");
+            List<students> slist = new List<students>()
+            {
+                //while creating the list the values are assigned
+                new students{Id=2,Name="Asif",cgpa=3.92},//0
+                new students{Id=3,Name="Alif",cgpa=3.40},//2
+                new students{Id=4,Name="Hemal",cgpa=3.77}//3
+                //cannot be removed 
+                //can e removed by removeAt()
+            };
+            slist.Insert(1, s);//1
+            //index,item
+            Console.WriteLine("Before remove...");
+            foreach (students i in slist)
+            {
+                //Console.WriteLine(i);//get addresses
+                Console.WriteLine("Id : " + i.Id + "Name : " + i.Name + "Cgpa : " + i.cgpa);
+            }
+            Console.ReadKey();
+
+            slist.Remove(s);
+            Console.WriteLine("After remove:");
+            foreach (students i in slist)
+            {
+
+                Console.WriteLine("Id : " + i.Id + "Name : " + i.Name + "Cgpa : " + i.cgpa);
+            }
+            slist.RemoveAt(0);
+            Console.WriteLine("After removeAt:");
+            foreach (students i in slist)
+            {
+
+                Console.WriteLine("Id : " + i.Id + "Name : " + i.Name + "Cgpa : " + i.cgpa);
+            }
+        }
+    }
+}
---
+++ Collections/Non-generic Collections/HashTable/HashTable/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+namespace HashTable
+{//Colections
+    //3 category
+    //1.Generic Colllection
+    //2.Non-Generic Colllection
+    //3.Concurrent Colllection
+
+
+    //non-generic collections->Arraylist,Queue,Stack,Hashtable
+    //Generic Collections->List,Queue,Stack,SortedList,Dictionary
+    //These are already implemented inside the System.collections namespace
+
+    //Non-Generic->Hashtable(key and value) 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht1 = new Hashtable();
+
+
+            //Add() method
+            ht1.Add(5, "Five");
+            ht1.Add(51, 65);
+            ht1.Add("Hello", "Bye");///key =hello; value=bye
+            ht1.Add("Hello1", 5.559);
+
+            //way-2 inserting
+            ht1[2] = "Good";//inside array notation is key and assigned is the value
+            //key=2;value="Good";
+            try
+            {
+                ht1.Add("Hello", 55);
+            }
+            catch
+            {
+                Console.WriteLine("Already Exist! Try Something new!");
+
+            }
+
+
+            //Accessing values
+            int a = (int)ht1[0];
+            int b = (int)ht1[1];
+
+            Console.WriteLine("Value of a:" + a);
+            Console.WriteLine("Value of b:" + b);
+
+
+            Console.WriteLine("printing values......");
+            //for hashtable we use dictionaryentry
+            foreach (DictionaryEntry i in ht1)
+            {
+                Console.WriteLine(i.Key + " " + i.Value);
+
+            }
+
+            //Containkey,Containvalue
+            Console.WriteLine("Key Availablity:" + ht1.ContainsKey(5));
+            Console.WriteLine("Key Availablity:" + ht1.ContainsValue("Five"));
+            Console.WriteLine();
+            //Remove elements
+            ht1.Remove("Hello");//Total will be removed
+            Console.WriteLine("After removing....");
+            foreach (DictionaryEntry i in ht1)
+            {
+                Console.WriteLine(i.Key + " " + i.Value);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
